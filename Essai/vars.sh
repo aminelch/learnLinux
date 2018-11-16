e214a5afe6f004993b2ac7ea9c103bb8a97a1cff
aminelch <aminelch95@gmail.com>
2018-11-16 16:39:28 +0100

    Decouvrir bash

---
+++ Essai/vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# Avec le  `` le contenu sera intrepeté comme une commande 
+# et le resultat sera stocké dans la variable repertoire 
+
+repertoire=`pwd`
+#On utilise echo pour afficher une chaine ou un var 
+#On procede le nom de variable d'un $ pour indiquer qu\'il avaluer la variable 
+
+
+echo  "Vous etes dans le dossier $repertoire " 
+
