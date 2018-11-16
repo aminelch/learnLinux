926570b7d099c9769620ed73578337ba4fb8fb71
Amine L'ch <aminelch95@gmail.com>
2018-11-16 16:50:21 +0100

    Message Prompt

---
+++ Essai/messagePrompt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# message de prompt
+# nous devrions afficher un message pour que l'utilisateur sache quoi faire. Avec l'option -p de read
+
+read -p  "entrer votre nom: " nom 
+echo "Bonjour $nom" 
+
