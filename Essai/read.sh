af21050aecb348789fc149781e3641752761ce4e
Amine L'ch <aminelch95@gmail.com>
2018-11-16 16:45:52 +0100

    le mot clé read

---
+++ Essai/read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+#read permet de lire un input et le stocké dans un var nom 
+echo "Bonjour, comment tu t'appelle tu ?"
+read nom
+#on n'oublie pas  de proceder le nom de var par un $ 
+echo "Bonjour $nom"
+
